971fa1f2ef9e0a62123070005bd1e62faccf750c
Форов Александр <5h9t5@list.ru>
2024-08-07 19:55:16 +0300

    feat(main): added alias router_meeting_room

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-from app.api.meeting_room import router
 from app.core.config import settings
+from app.api.meeting_room import router as router_meeting_room
 
 
 app = FastAPI(title=settings.app_title)
 
-app.include_router(router)
+app.include_router(router_meeting_room)
