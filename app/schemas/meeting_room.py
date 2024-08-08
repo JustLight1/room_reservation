98e666d5df39a9781dbbc2d724f5f3124a521c56
Форов Александр <5h9t5@list.ru>
2024-08-08 17:54:44 +0300

    feat(schemas/meeting_room): added MeetingRoomBase: base class, MeetingRoomCreate: class for post method, MeetingRoomDB: class for response

--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -1,8 +1,17 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 
 
-class MeetingRoomCreate(BaseModel):
-    name: str = Field(..., max_length=100)
-    description: Optional[str]
+class MeetingRoomBase(BaseModel):
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
+    description: Optional[str] = None
+
+
+class MeetingRoomCreate(MeetingRoomBase):
+    name: str = Field(..., min_length=1, max_length=100)
+
+
+class MeetingRoomDB(MeetingRoomCreate):
+    id: int
+    model_config = ConfigDict(from_attributes=True)
