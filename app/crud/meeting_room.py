5f2dae721d9ea9ea06a2b1b710f7ba28737c4a72
Форов Александр <5h9t5@list.ru>
2024-08-09 17:50:54 +0300

    feat(crud/meeting_room): added get_meeting_room_by_id: -> get room by id and added update_meeting_room: -> update room data

--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -1,10 +1,12 @@
 from typing import Optional
 
+from fastapi.encoders import jsonable_encoder
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.meeting_room import MeetingRoom
-from app.schemas.meeting_room import MeetingRoomCreate
+from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomUpdate
 
 
 async def read_all_rooms_from_db(session: AsyncSession) -> list[MeetingRoom]:
@@ -35,3 +37,31 @@ async def get_room_id_by_name(
     )
     db_room_id = db_room_id.scalars().first()
     return db_room_id
+
+
+async def get_meeting_room_by_id(
+        room_id: int,
+        session: AsyncSession,
+) -> Optional[MeetingRoom]:
+    db_room = await session.execute(
+        select(MeetingRoom).where(MeetingRoom.id == room_id)
+    )
+    db_room = db_room.scalars().first()
+    return db_room
+
+
+async def update_meeting_room(
+        db_room: MeetingRoom,
+        room_in: MeetingRoomUpdate,
+        session: AsyncSession,
+) -> MeetingRoom:
+    obj_data = jsonable_encoder(db_room)
+    update_data = room_in.model_dump(exclude_unset=True)
+
+    for field in obj_data:
+        if field in update_data:
+            setattr(db_room, field, update_data[field])
+    session.add(db_room)
+    await session.commit()
+    await session.refresh(db_room)
+    return db_room
