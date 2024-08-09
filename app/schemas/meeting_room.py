b49f2509031c88cd11b6e589188b00ac86bb0861
Форов Александр <5h9t5@list.ru>
2024-08-09 17:52:17 +0300

    feat(schemas/meeting_room): added MeetingRoomUpdate: -> for update data and added field_validator: name is None

--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 
 class MeetingRoomBase(BaseModel):
@@ -10,6 +10,16 @@ class MeetingRoomBase(BaseModel):
 
 class MeetingRoomCreate(MeetingRoomBase):
     name: str = Field(..., min_length=1, max_length=100)
+
+
+class MeetingRoomUpdate(MeetingRoomBase):
+
+    @field_validator('name')
+    @classmethod
+    def name_cannot_be_null(cls, value):
+        if value is None:
+            raise ValueError('Имя переговорки не может быть пустым!')
+        return value
 
 
 class MeetingRoomDB(MeetingRoomCreate):
