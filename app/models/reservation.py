713c9ef2e2b553135266e55660fad7f3a5ea6564
Форов Александр <5h9t5@list.ru>
2024-08-10 20:54:16 +0300

    chore(models/reservation): added annotates

--- app/models/reservation.py
+++ app/models/reservation.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer
+from sqlalchemy import DateTime, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.db import Base
 
 
 class Reservation(Base):
-    from_reserve = Column(DateTime)
-    to_reserve = Column(DateTime)
-    meetingroom_id = Column(Integer, ForeignKey('meetingroom.id'))
+    from_reserve: Mapped[DateTime] = mapped_column(DateTime)
+    to_reserve: Mapped[DateTime] = mapped_column(DateTime)
+    meetingroom_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey('meetingroom.id')
+    )
