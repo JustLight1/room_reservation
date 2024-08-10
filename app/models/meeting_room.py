78f69e8a6e9c3a591302b9bb724645d7f8e34818
Форов Александр <5h9t5@list.ru>
2024-08-10 20:53:45 +0300

    refactor(models/meeting_room): added annotates

--- app/models/meeting_room.py
+++ app/models/meeting_room.py
@@ -1,10 +1,17 @@
-from sqlalchemy import Column, String, Text
-from sqlalchemy.orm import relationship
+from typing import TYPE_CHECKING, List
+
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.core.db import Base
 
 
+if TYPE_CHECKING:
+    from app.models.reservation import Reservation
+
+
 class MeetingRoom(Base):
-    name = Column(String(100), unique=True, nullable=False)
-    description = Column(Text)
-    reservations = relationship('Reservation', cascade='delete')
+    name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(Text)
+    reservations: Mapped[List['Reservation']] = relationship(
+        'Reservation', cascade='delete')
