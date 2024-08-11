2ec60f8c4edcb9ec6f173f832949cedbf97d990c
Форов Александр <5h9t5@list.ru>
2024-08-11 17:00:42 +0300

    refactor(main): update import routers and include_router

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
 from app.core.config import settings
-from app.api.meeting_room import router as router_meeting_room
+from app.api.routers import main_router
 
 
 app = FastAPI(title=settings.app_title)
 
-app.include_router(router_meeting_room)
+app.include_router(main_router)
