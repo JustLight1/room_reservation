0887e196ccd8705dfdd1cff3d4a629f0d0842294
Форов Александр <5h9t5@list.ru>
2024-08-12 17:17:02 +0300

    feat(api/reservation): added methods get: get_all_reservations, patch: update_reservation and delete: delete_reservation

--- app/api/endpoints/reservation.py
+++ app/api/endpoints/reservation.py
@@ -5,17 +5,30 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.db import get_async_session
 from app.crud.reservation import reservation_crud
 from app.api.validators import (
-    check_meeting_room_exists, check_reservation_intersections
+    check_meeting_room_exists,
+    check_reservation_before_edit,
+    check_reservation_intersections,
 )
-from app.schemas.reservation import ReservationCreate, ReservationDB
+from app.schemas.reservation import (
+    ReservationCreate, ReservationDB, ReservationUpdate
+)
+
 
 router = APIRouter()
+
+
+@router.get('/', response_model=list[ReservationDB])
+async def get_all_reservations(
+    session: AsyncSession = Depends(get_async_session)
+):
+    reservations = await reservation_crud.get_multi(session)
+    return reservations
 
 
 @router.post('/', response_model=ReservationDB)
 async def create_reservation(
     reservation: ReservationCreate,
-    session: AsyncSession = Depends(get_async_session)
+    session: AsyncSession = Depends(get_async_session),
 ):
     await check_meeting_room_exists(reservation.meetingroom_id, session)
     await check_reservation_intersections(
@@ -25,3 +38,40 @@ async def create_reservation(
         reservation, session
     )
     return new_reservation
+
+
+@router.patch('/{reservation_id}', response_model=ReservationDB)
+async def update_reservation(
+    reservation_id: int,
+    obj_in: ReservationUpdate,
+    session: AsyncSession = Depends(get_async_session)
+):
+    reservation = await check_reservation_before_edit(
+        reservation_id, session
+    )
+    await check_reservation_intersections(
+        **obj_in.model_dump(),
+        reservation_id=reservation_id,
+        meetingroom_id=reservation.meetingroom_id,
+        session=session
+    )
+    reservation = await reservation_crud.update(
+        db_obj=reservation,
+        obj_in=obj_in,
+        session=session
+    )
+    return reservation
+
+
+@router.delete('/{reservation_id}', response_model=ReservationDB)
+async def delete_reservation(
+    reservation_id: int,
+    session: AsyncSession = Depends(get_async_session)
+):
+    reservation = await check_reservation_before_edit(
+        reservation_id, session
+    )
+    reservation = await reservation_crud.remove(
+        reservation, session
+    )
+    return reservation
