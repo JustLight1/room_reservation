f926e854d3a3f9adf1a17eab0d221a699c5e3162
Форов Александр <5h9t5@list.ru>
2024-08-13 18:16:00 +0300

    feat(core/config): added first_superuser_email and first_superuser_password

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,14 @@
+from pydantic import EmailStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
-    description: str
+    app_description: str
     database_url: str
-    secret: str = 'SECRET'
+    secret: str = 'secret'
+    first_superuser_email: EmailStr | None = None
+    first_superuser_password: str | None = None
 
     model_config = SettingsConfigDict(env_file='.env')
 
