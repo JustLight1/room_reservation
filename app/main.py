0eb7de2228f18e73d5d55526e1661387416b58ff
Форов Александр <5h9t5@list.ru>
2024-08-13 18:12:11 +0300

    feat(main): added lifespan

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.core.config import settings
 from app.api.routers import main_router
+from app.core.init_db import create_first_superuser
 
 
-app = FastAPI(title=settings.app_title)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_first_superuser()
+    yield
+
+
+app = FastAPI(
+    title=settings.app_title,
+    description=settings.app_description,
+    lifespan=lifespan
+)
 
 app.include_router(main_router)
