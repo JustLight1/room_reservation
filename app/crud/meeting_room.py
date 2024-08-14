659b1446e8f94aad28fc0541458c2414dd3d42c5
Форов Александр <5h9t5@list.ru>
2024-08-14 17:20:38 +0300

    docs(main): added docstring

--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -11,12 +11,26 @@ class CRUDMeetingRoom(CRUDBase[
     MeetingRoomCreate,
     MeetingRoomUpdate
 ]):
+    """
+    Класс для операций CRUD с моделью MeetingRoom.
+    """
 
     async def get_room_id_by_name(
             self,
             room_name: str,
             session: AsyncSession,
     ) -> int | None:
+        """
+        Получает идентификатор комнаты по ее названию.
+
+        Args:
+            room_name (str): Название комнаты.
+            session (AsyncSession): Асинхронная сессия SQLAlchemy.
+
+        Returns:
+            int or None: Идентификатор найденной комнаты или None,
+                         если комната не найдена.
+        """
         db_room_id = await session.execute(
             select(MeetingRoom.id).where(
                 MeetingRoom.name == room_name
