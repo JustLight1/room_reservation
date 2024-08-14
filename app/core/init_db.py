d502893ea6ca92edcf6a31f820c114b554698fb9
Форов Александр <5h9t5@list.ru>
2024-08-14 17:20:28 +0300

    docs(main): added docstring

--- app/core/init_db.py
+++ app/core/init_db.py
@@ -17,6 +17,18 @@ get_user_manager_context = contextlib.asynccontextmanager(get_user_manager)
 async def create_user(
         email: EmailStr, password: str, is_superuser: bool = False
 ):
+    """
+    Создает нового пользователя в базе данных.
+
+    Args:
+        email (EmailStr): Email пользователя.
+        password (str): Пароль пользователя.
+        is_superuser (bool, default = False): Является ли пользователь
+        суперпользователем. По умолчанию False.
+
+    Raises:
+        UserAlreadyExists: Если пользователь с таким email уже существует.
+    """
     try:
         async with get_async_session_context() as session:
             async with get_user_db_context(session) as user_db:
@@ -33,6 +45,10 @@ async def create_user(
 
 
 async def create_first_superuser():
+    """
+    Создает первого суперпользователя, если указаны его email и пароль
+    в настройках.
+    """
     if (settings.first_superuser_email is not None
             and settings.first_superuser_password is not None):
         await create_user(
